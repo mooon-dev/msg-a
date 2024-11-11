e5482a8a86013347d47b72515fc3fb906c1d0ea1
mooon-dev <zainab.42847@gmail.com>
2024-11-11 09:06:40 -0800

    😛

---
+++ client/src/utils/APIRoutes.js
@@ -0,0 +1,11 @@
+export const host = "https://kona-a8bn.onrender.com";
+
+export const registerRoute = `${host}/api/auth/register`;
+export const loginRoute = `${host}/api/auth/login`;
+export const setAvatarRoute = `${host}/api/auth/setAvatar`;
+export const allUsersRoute = `${host}/api/auth/allusers`;
+export const logoutRoute = `${host}/api/auth/logout`;
+export const sendMessageRoute = `${host}/api/messages/addmsg`;
+export const getAllMessagesRoute = `${host}/api/messages/getmsg`;
+
+
